c7e56db4d9e62dcfbbabe597ec28072380fac8d7
Olav Valle <olavval@stud.ntnu.no>
2019-10-01 17:43:09 +0200

    add working branch, add doc directory

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Book"},{"p":"<Unnamed>","l":"BookLibApp"},{"p":"<Unnamed>","l":"InputReader"},{"p":"<Unnamed>","l":"Library"},{"p":"<Unnamed>","l":"Search"}]
