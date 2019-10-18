73eac7774bda5f454baca157b2b08f23b51af61d
Olav Valle <olavval@stud.ntnu.no>
2019-10-18 20:09:38 +0200

    add ISBN-10 check digit algorithm

--- app/ISBNChecker.java
+++ app/ISBNChecker.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.stream.Collectors;
@@ -11,24 +12,13 @@ import static java.util.Arrays.asList;
  */
 public class ISBNChecker {
 
-    Integer[] ISBN13prefix = {9, 7, 8};
+    private static final Integer[] ISBN13prefix = {9, 7, 8}; //Bookland UCC
 
     /**
      * Constructor.
      */
     public ISBNChecker() {
-    }
 
-    /**
-     * Separates a long type number into individual integers, and returns them in an ArrayList.
-     *
-     * @param longNumber A number of primitive type long.
-     * @return An ArrayList collecting the individual integer digits of longNumber.
-     */
-    public ArrayList<Integer> ISBNLongToIntArray(long longNumber) {
-        return Arrays.stream(Long.toString(longNumber)
-                .split("")).map(Integer::parseInt)
-                .collect(Collectors.toCollection(ArrayList::new));
     }
 
     /**
@@ -39,7 +29,7 @@ public class ISBNChecker {
      */
     public long convert10to13(long isbn10)
     {
-        ArrayList<Integer> intList = ISBNLongToIntArray(isbn10); //convert the long to list of int
+        ArrayList<Integer> intList = ISBNLongToIntArrayList(isbn10); //convert the long to list of int
 
         intList.remove(9); //remove ISBN-10 check digit
 
@@ -54,16 +44,68 @@ public class ISBNChecker {
     }
 
     /**
+     * Verifies an ISBN number by testing its check digit.
+     * Compares the check digit (the last digit) of the ISBN number, to the digit calculated from the check digit algorithm.
+     * @param isbn An ISBN number of type long.
+     * @return true if number is a valid ISBN, false if not.
+     */
+    public boolean verifyISBN(long isbn)
+    {
+        boolean valid = false;
+
+        ArrayList<Integer> isbnAsInt = ISBNLongToIntArrayList(isbn);
+
+        if(isbnAsInt.size() == 13) {
+            valid = (isbnAsInt.get(12) == calculateISBN13CheckDigit(isbnAsInt));
+        }
+        else if(isbnAsInt.size() == 10) {
+            valid = (isbnAsInt.get(9) == calculateISBN10CheckDigit(isbnAsInt));
+        //TODO implement ISBN-10 check digit algorithm
+        }
+
+        return valid;
+    }
+
+    /**
+     * Separates a long type number into individual integers, and returns them in an ArrayList.
+     *
+     * @param longNumber A number of primitive type long.
+     * @return An ArrayList collecting the individual integer digits of longNumber.
+     */
+    public ArrayList<Integer> ISBNLongToIntArrayList(long longNumber) {
+        return Arrays.stream(Long.toString(longNumber)
+                .split("")).map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    /**
      * Calculates the check digit of a 13 digit ISBN number using the first 12 digits.
      *
-     * @param first12Digits An ArrayList of the first 12 digits in an ISBN-13 number.
+     * @param isbn13Ints An ArrayList of the first 12 digits in an ISBN-13 number.
      * @return The calculated check digit.
      */
-    private int calculateISBN13CheckDigit(ArrayList<Integer> first12Digits)
+    private int calculateISBN13CheckDigit(ArrayList<Integer> isbn13Ints)
     {
-        int checkDigit13 = (10 - ((first12Digits.get(0) + 3 * first12Digits.get(1) + first12Digits.get(2) + 3 * first12Digits.get(3) + first12Digits.get(4) + 3 * first12Digits.get(5)
-                + first12Digits.get(6) + 3 * first12Digits.get(7) + first12Digits.get(8) + 3 * first12Digits.get(9) + first12Digits.get(10) + 3 * first12Digits.get(11))) % 10);
+        int checkDigit13 = (10 - ((isbn13Ints.get(0) + 3 * isbn13Ints.get(1) + isbn13Ints.get(2) + 3 * isbn13Ints.get(3) + isbn13Ints.get(4) + 3 * isbn13Ints.get(5)
+                + isbn13Ints.get(6) + 3 * isbn13Ints.get(7) + isbn13Ints.get(8) + 3 * isbn13Ints.get(9) + isbn13Ints.get(10) + 3 * isbn13Ints.get(11))) % 10);
         return checkDigit13;
     }
 
+    /**
+     * Calculates the check digit of a 10 digit ISBN number using the first 9 digits.
+     * @param isbn10Ints An array of the first 9 digits in the
+     * @return The calculated check digit.
+     */
+    public int calculateISBN10CheckDigit(ArrayList<Integer> isbn10Ints)
+    {
+        int sum = 0;
+
+
+        for(int i = 0; i < 9; i++){
+            sum += ((10-i) * isbn10Ints.get(i));
+        }
+        return (11 - (sum % 11) ) % 11;
+
+
+    }
 }
--- test/ISBNTest.java
+++ test/ISBNTest.java
@@ -1,13 +1,17 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+public class ISBNTest {
 
-public class ISBNTest<ISBN10> {
-
-    private long ISBN10;
-    private long ISBN13;
+    private long isbn10;
+    private long isbn13;
+    private long sinus13;
+    private long linear10;
+    private long linear13;
+    private long electric10;
+    private long electric13;
     private ISBNChecker testISBNChecker;
 
     public ISBNTest()
@@ -15,14 +19,45 @@ public class ISBNTest<ISBN10> {
     }
     @BeforeEach
     public void setUp() {
-        ISBN10 = 1292159049L;
-        ISBN13 = 9781292159041L;
+
+        //ISBN-10
+        isbn10 =    1292159049L;
+        linear10 =  1292092238L;
+        //electric10 = 0_132_066_920L;
+
+        //ISBN-13
+        isbn13 =     9781292159041L;
+        sinus13 =    9788202509057L;
+        linear13 =   9781292092232L;
+        electric13 = 9780132066921L;
+
         testISBNChecker = new ISBNChecker();
     }
 
     @Test
     public void test10to13()
     {
-        assertEquals(ISBN13, testISBNChecker.convert10to13(ISBN10));
+        assertEquals(isbn13, testISBNChecker.convert10to13(isbn10));
+        assertEquals(linear13, testISBNChecker.convert10to13(linear10));
+    }
+
+    @Test
+    public void testVerifyISBN()
+    {
+        assertTrue(testISBNChecker.verifyISBN(linear10));
+        assertTrue(testISBNChecker.verifyISBN(isbn10));
+        assertTrue(testISBNChecker.verifyISBN(isbn13));
+        assertTrue(testISBNChecker.verifyISBN(sinus13));
+        assertTrue(testISBNChecker.verifyISBN(linear13));
+        assertTrue(testISBNChecker.verifyISBN(electric13));
+    }
+
+    @Test
+    public void test10CheckDigit()
+    {
+        assertEquals(9, testISBNChecker.calculateISBN10CheckDigit(testISBNChecker.ISBNLongToIntArrayList(isbn10)));
+
+        assertEquals(8, testISBNChecker.calculateISBN10CheckDigit(testISBNChecker.ISBNLongToIntArrayList(linear10)));
+
     }
 }
